bae66b2c6a718245e9c5854abcd26ecebf099c44
Mostafaawwad7 <mostafaawad311@gmail.com>
2022-09-09 09:47:59 -0700

    sd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+   * main - print alphabet in upper and lower
+   *
+   * Return: zero
+   */
+int main(void)
+{
+	int i;
+	
+	for (i=0 ; i<=9 ; i++)
+	{
+		print("%d", i);
+	}
+	print("\n");
+
+	return (0);
+}
