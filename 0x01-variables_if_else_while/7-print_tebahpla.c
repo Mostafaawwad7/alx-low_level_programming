d74288d5101b6883de070562efeba04d044b00e2
Mostafaawwad7 <mostafaawad311@gmail.com>
2022-09-09 10:31:42 -0700

    sd

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,20 @@
    */
 int main(void)
 {
+	int i = 0;
+	char alphabet[26];
 	char ch;
 
-	for (ch = 'z'; ch <= 'a'; ch--)
-		putchar(ch);
+	for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        alphabet[i] = ch;
+        i++;
+    }
+    for (int j = sizeof(alphabet) ; j >= 0; j--)
+    {
+        printf("%c",alphabet[j]);
+    }
+
 	putchar('\n');
 	return (0);
 }
