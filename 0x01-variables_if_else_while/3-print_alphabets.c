c789ed06c18d9fe68a7572ae7ae57696a16c78a4
Mostafaawwad7 <mostafaawad311@gmail.com>
2022-09-09 09:03:22 -0700

    sd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -23,24 +22,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-=======
-#include <stdio.h>
-
-/**
-   * main - print alphabet in upper and lower
-   *
-   * Return: zero
-   */
-int main(void)
-{
-	char ch;
-	char CH;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (CH = 'A'; CH <= 'Z'; CH++)
-		putchar(CH);
-	putchar('\n');
-	return (0);
-}
->>>>>>> fc5c65b8dcedaeff6bf69f6f3661b7bc4f7a9019
