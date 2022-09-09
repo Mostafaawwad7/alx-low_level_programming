8fb6ccfb29c1e9696e739c5c2b69167519e88a3d
Mostafaawwad7 <mostafaawad311@gmail.com>
2022-09-09 10:44:04 -0700

    sd

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
 /**
-   * main - print alphabet in lowercase
+   * main - prints alphabet in reverse
    *
-   *Return: zero
+   * Return: zero
    */
 int main(void)
 {
-	int i = 0;
-	char alphabet[26];
 	char ch;
-	int j;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		alphabet[i] = ch;
-		i++;
-	}
-	for (j = sizeof(alphabet) ; j >= 0 ; j--)
-	{
-		printf("%c",alphabet[j]);
-	}
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
