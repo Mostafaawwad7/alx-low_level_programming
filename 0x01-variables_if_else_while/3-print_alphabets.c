fc5c65b8dcedaeff6bf69f6f3661b7bc4f7a9019
Mostafaawwad7 <105162503+Mostafaawwad7@users.noreply.github.com>
2022-09-09 17:51:02 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,18 +7,17 @@
    */
 int main(void)
 {
-	char ch;
-  char ch2;
+char ch;
+char ch2;
 
 	for (ch = 'a' ; ch <= 'z'; ch++)
 	{
-		putchar(ch);
-    
-	}
-  for ( ch2 = 'A' ; ch2 <= 'Z' ; ch2++)
-  {
-    putchar(ch2);
-  }
+	putchar(ch);
+    	}
+  	for ( ch2 = 'A' ; ch2 <= 'Z' ; ch2++)
+  	{
+    	putchar(ch2);
+  	}
 	putchar('\n');
 	return (0);
 }
