8cd2ac2cda114c2fec05432027cd03308079e360
Mostafaawwad7 <mostafaawad311@gmail.com>
2022-09-13 06:44:12 -0700

    sd

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _islower - checks for lowercase or uppercase characters.
@@ -9,11 +9,7 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
