cc164c09f38d7c82c5713f04f9200d077d481cc2
Mostafaawwad7 <mostafaawad311@gmail.com>
2022-09-19 15:45:14 -0700

    sd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,44 +1,39 @@
 #include "main.h"
 /**
- * puts_half - print half of a string.
- * @str: character
- */
+*puts_half - puts half of a string
+*@str: string
+*Return:void
+*/
 void puts_half(char *str)
 {
-	int m, n, i, j;
+int len = _strlen(str);
 
-	m = string_length(str) / 2;
+if (len % 2 == 0)
+len = len / 2;
 
-	if (string_length(str) % 2 == 0)
-	{
-		for (i = m; i < string_length(str); i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else if (string_length(str) % 2 != 0)
-	{
-		n = (string_length(str) - 1) / 2;
-		for (j = n; j <= string_length(str); j++)
-		{
-			_putchar(str[j]);
-		}
-	}
-	_putchar('\n');
+else
+len = (len + 1) / 2;
+
+while (str[len] != '\0')
+{
+_putchar(str[len]);
+len++;
+}
+_putchar('\n');
 }
 
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
 
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
 }
